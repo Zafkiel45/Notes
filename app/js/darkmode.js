c777cb5cf6b21c1d0ddf0586c52788c4493efed7
zafkiel45 <zafkielc@gmail.com>
2024-05-26 11:49:33 -0300

    feat:text-area

--- app/js/darkmode.js
+++ app/js/darkmode.js
@@ -1,4 +1,5 @@
 const body = document.querySelector('.container_master');
+const textarea = document.querySelector('#content');
 let darkmode;
 export function checkMode() {
     console.log('checked');
@@ -12,17 +13,25 @@ export function checkMode() {
         document.documentElement.classList.add('light');
         darkmode = false;
     }
+    updateElements();
 }
 export function switchMode() {
     console.log("swichted");
     darkmode = !darkmode;
+    updateElements();
+}
+function updateElements() {
     if (darkmode) {
         body.classList.remove('container_master');
         body.classList.add('container_master_darkmode');
+        textarea.classList.remove('text_area');
+        textarea.classList.add('text_area_darkmode');
     }
     else {
         body.classList.remove('container_master_darkmode');
         body.classList.add('container_master');
+        textarea.classList.remove('text_area_darkmode');
+        textarea.classList.add('text_area');
     }
 }
 //# sourceMappingURL=darkmode.js.map
--- app/typescript/darkmode.ts
+++ app/typescript/darkmode.ts
@@ -1,4 +1,5 @@
 const body = document.querySelector('.container_master') as HTMLBodyElement;
+const textarea = document.querySelector('#content') as HTMLTextAreaElement ;
 let darkmode: boolean | undefined;
 
 export function checkMode():void {
@@ -12,16 +13,24 @@ export function checkMode():void {
         document.documentElement.classList.add('light');
         darkmode = false;
     }
+    updateElements()
 }
 
 export function switchMode() {
     console.log("swichted")
     darkmode = !darkmode;
+    updateElements()
+}
+function updateElements():void {
     if(darkmode) {
         body.classList.remove('container_master');
         body.classList.add('container_master_darkmode');
+        textarea.classList.remove('text_area');
+        textarea.classList.add('text_area_darkmode');
     } else {
         body.classList.remove('container_master_darkmode');
         body.classList.add('container_master');
+        textarea.classList.remove('text_area_darkmode');
+        textarea.classList.add('text_area');
     }
 }
