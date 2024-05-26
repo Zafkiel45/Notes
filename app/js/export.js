6a55526b7586ae9aff75e326bb7af4c4c4d5c522
zafkiel45 <zafkielc@gmail.com>
2024-05-26 15:20:47 -0300

    fix: fixed titles

--- app/js/export.js
+++ app/js/export.js
@@ -1,9 +1,9 @@
 "use strict";
-const textarea = document.querySelector('#content');
+const textarea = document.querySelector("#content");
 const link = document.querySelector("#link");
-const export_button = document.querySelector('#export_button');
+const export_button = document.querySelector("#export_button");
 let content;
-textarea.addEventListener('change', (event) => {
+textarea.addEventListener("change", (event) => {
     const targetElement = event.target;
     content = targetElement.value;
 });
@@ -12,21 +12,27 @@ function Formater() {
         return text.replace(pattern, replacement);
     }
     function titleConversion(text, pattern, replacement) {
-        return text.replace(pattern, function (match, p1) { return replacement + 1; });
+        return text.replace(pattern, function (match) {
+            const matchResult = match.trim().match(/^#+/);
+            if (matchResult) {
+                const level = matchResult[0].length;
+                return `${replacement.repeat(level)} ${match.trim().substring(level).trim()}`;
+            }
+            return match;
+        });
     }
-    let markdown = conversion(content, /\*\*(.*?)\*\*/g, '**$1**');
-    markdown = conversion(markdown, /__(.*?)__/g, '__$1__');
-    markdown = titleConversion(markdown, /^#{1,6}\s[a-zA-Z0-9\s\-\_\.,]+\s*$/gm, '');
+    let markdown = conversion(content, /\*\*(.*?)\*\*/g, "**$1**");
+    markdown = conversion(markdown, /__(.*?)__/g, "__$1__");
+    markdown = titleConversion(markdown, /^#{1,6}\s[a-zA-Z0-9\s\-\_\.,]+\s*$/gm, "#");
     let blob = new Blob([markdown], { type: "text/markdown;charset=utf-8" });
     let url = URL.createObjectURL(blob);
     link.href = url;
-    link.download = 'document.md';
-    link.click();
+    link.download = "document.md";
     setTimeout(() => {
         window.URL.revokeObjectURL(url);
     }, 100);
 }
-export_button.addEventListener('click', () => {
+export_button.addEventListener("click", () => {
     Formater();
 });
 //# sourceMappingURL=export.js.map
--- app/typescript/export.ts
+++ app/typescript/export.ts
@@ -1,37 +1,49 @@
-const textarea = document.querySelector('#content') as HTMLTextAreaElement;
-const link = document.querySelector("#link") as HTMLAnchorElement ;
-const export_button = document.querySelector('#export_button') as HTMLButtonElement;
+const textarea = document.querySelector("#content") as HTMLTextAreaElement;
+const link = document.querySelector("#link") as HTMLAnchorElement;
+const export_button = document.querySelector(
+  "#export_button"
+) as HTMLButtonElement;
 let content: string;
 
-textarea.addEventListener('change', (event: Event) => {
-    const targetElement = event.target as HTMLTextAreaElement;
-    content = targetElement.value;
-})
+textarea.addEventListener("change", (event: Event) => {
+  const targetElement = event.target as HTMLTextAreaElement;
+  content = targetElement.value;
+});
 
-function Formater() {
-    function conversion(text:string, pattern:RegExp, replacement:string) {
-        return text.replace(pattern, replacement);
-    }
-    function titleConversion(text:string, pattern:RegExp, replacement:string) {
-        return text.replace(pattern, function(match, p1) {return replacement + 1})
-    }
+function Formater():void {
+  function conversion(text: string, pattern: RegExp, replacement: string):string {
+    return text.replace(pattern, replacement);
+  }
+  function titleConversion(text: string, pattern: RegExp, replacement: string):string {
+    return text.replace(pattern, function (match:string) {
+        const matchResult = match.trim().match(/^#+/);
+        if (matchResult) {
+          const level = matchResult[0].length;  // Conta o número de '#'
+          return `${replacement.repeat(level)} ${match.trim().substring(level).trim()}`;
+        }
+        return match;  // Retorna o match original se não encontrar '#'
+    });
+  }
 
-    let markdown = conversion(content, /\*\*(.*?)\*\*/g, '**$1**');
-    markdown = conversion(markdown, /__(.*?)__/g, '__$1__');
-    markdown = titleConversion(markdown, /^#{1,6}\s[a-zA-Z0-9\s\-\_\.,]+\s*$/gm, '');
+  let markdown = conversion(content, /\*\*(.*?)\*\*/g, "**$1**");
+  markdown = conversion(markdown, /__(.*?)__/g, "__$1__");
+  markdown = titleConversion(
+    markdown,
+    /^#{1,6}\s[a-zA-Z0-9\s\-\_\.,]+\s*$/gm,
+    "#"
+  );
 
-    let blob = new Blob([markdown], {type: "text/markdown;charset=utf-8"});
-    let url = URL.createObjectURL(blob);
+  let blob:Blob = new Blob([markdown], { type: "text/markdown;charset=utf-8" });
+  let url = URL.createObjectURL(blob);
 
-    link.href = url;
-    link.download = 'document.md';
-    link.click();
+  link.href = url;
+  link.download = "document.md";
 
-    setTimeout(() => {
-        window.URL.revokeObjectURL(url);
-    }, 100)
+  setTimeout(() => {
+    window.URL.revokeObjectURL(url);
+  }, 100);
 }
 
-export_button.addEventListener('click', () => {
-    Formater();
-})
+export_button.addEventListener("click", () => {
+  Formater();
+});
