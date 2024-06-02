e37590022b57d330cc47d7f478c5aa3924c10a22
zafkiel45 <zafkielc@gmail.com>
2024-06-02 17:43:31 -0300

    feat: table button

--- app/js/formater_buttons.js
+++ app/js/formater_buttons.js
@@ -1,8 +1,9 @@
-import { DeleteElement, HandleEditorElements, } from "./editor.js";
+import { DeleteElement, HandleEditorElements, handleClearPaste } from "./editor.js";
 const bold_btn = document.querySelector("#bold_button");
 const italic_btn = document.querySelector("#italic_button");
 const list_btn = document.querySelector("#list_button");
 const code_btn = document.querySelector("#code_button");
+const table_btn = document.querySelector("#table_button");
 const main = document.querySelector('#container_main');
 code_btn.addEventListener("click", () => {
     const NewPre = document.createElement('pre');
@@ -11,6 +12,7 @@ code_btn.addEventListener("click", () => {
     NewPre.className = 'code_block editable';
     NewPre.contentEditable = 'true';
     NewPre.textContent = '```javascript\n\n\n```';
+    NewPre.spellcheck = false;
     NewPre.addEventListener('keydown', HandleEditorElements);
     NewPre.addEventListener('keydown', DeleteElement);
     elementsWithContentEditable.forEach((item) => {
@@ -18,6 +20,27 @@ code_btn.addEventListener("click", () => {
             TargetElement = item;
             TargetElement.insertAdjacentElement("afterend", NewPre);
             NewPre.focus();
+        }
+    });
+});
+table_btn.addEventListener('click', () => {
+    const NewDiv = document.createElement('div');
+    const elementsWithContentEditable = document.querySelectorAll("div[contenteditable]");
+    NewDiv.className = 'table editable';
+    NewDiv.contentEditable = 'true';
+    NewDiv.textContent = `| Header 1 | Header 2 |
+  | -------- | -------- |
+  | Item 1   | item 2   |
+  `;
+    let TargetElement;
+    NewDiv.addEventListener('keydown', HandleEditorElements);
+    NewDiv.addEventListener('keydown', DeleteElement);
+    NewDiv.addEventListener('paste', handleClearPaste);
+    elementsWithContentEditable.forEach((item) => {
+        if (item.classList.contains('selected')) {
+            TargetElement = item;
+            TargetElement.insertAdjacentElement("afterend", NewDiv);
+            NewDiv.focus();
         }
     });
 });
--- app/typescript/formater_buttons.ts
+++ app/typescript/formater_buttons.ts
@@ -1,12 +1,14 @@
 import { 
     DeleteElement,
     HandleEditorElements,
+    handleClearPaste
 } from "./editor.js";
 
 const bold_btn = document.querySelector("#bold_button") as HTMLButtonElement;
 const italic_btn = document.querySelector("#italic_button") as HTMLButtonElement;
 const list_btn = document.querySelector("#list_button") as HTMLButtonElement;
 const code_btn = document.querySelector("#code_button") as HTMLButtonElement;
+const table_btn = document.querySelector("#table_button") as HTMLButtonElement;
 const main = document.querySelector('#container_main') as HTMLDivElement;
 
 code_btn.addEventListener("click", () => {
@@ -20,6 +22,7 @@ code_btn.addEventListener("click", () => {
     NewPre.className = 'code_block editable';
     NewPre.contentEditable = 'true';
     NewPre.textContent = '```javascript\n\n\n```'
+    NewPre.spellcheck = false;
 
     NewPre.addEventListener('keydown', HandleEditorElements);
     NewPre.addEventListener('keydown', DeleteElement);
@@ -35,3 +38,34 @@ code_btn.addEventListener("click", () => {
       }
     })
 });
+table_btn.addEventListener('click', () => {
+  const NewDiv = document.createElement('div');
+  const elementsWithContentEditable = document.querySelectorAll<HTMLDivElement>(
+    "div[contenteditable]"
+  );
+
+  NewDiv.className = 'table editable';
+  NewDiv.contentEditable = 'true';
+  NewDiv.textContent = `| Header 1 | Header 2 |
+  | -------- | -------- |
+  | Item 1   | item 2   |
+  `
+
+  let TargetElement: Element;
+
+  NewDiv.addEventListener('keydown', HandleEditorElements);
+  NewDiv.addEventListener('keydown', DeleteElement);
+  NewDiv.addEventListener('paste', handleClearPaste);
+
+  elementsWithContentEditable.forEach((item) => {
+    if(item.classList.contains('selected')) {
+      TargetElement = item;
+      (TargetElement as HTMLDivElement).insertAdjacentElement(
+        "afterend",
+        NewDiv
+      );
+      NewDiv.focus();
+    }
+  })
+
+})
